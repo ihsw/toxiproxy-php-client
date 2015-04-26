3978cf422b1788077bbcfab5a442830723d1d646
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-04-26 15:04:03 -0400

    - initial commit

---
+++ src/Toxiproxy.php
@@ -0,0 +1,9 @@
+<?php namespace Ihsw\Toxiproxy;
+
+class Toxiproxy
+{
+    public function hasDerp($derp = true)
+    {
+        return $derp;
+    }
+}
---
+++ tests/ToxiproxyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Ihsw\Toxiproxy\Toxiproxy;
+
+class ToxiproxyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasDerp()
+    {
+        $toxiproxy = new Toxiproxy();
+        $this->assertTrue($toxiproxy->hasDerp());
+    }
+}
