9f8cee6b800d7c01730a3dfed247681ab4a46949
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-04-29 18:57:26 -0400

    - removing unused

--- src/Exception/ProxyExistsException.php
+++ src/Exception/ProxyExistsException.php
@@ -1,11 +1,3 @@
 <?php namespace Ihsw\Toxiproxy\Exception;
 
-class ProxyExistsException extends \RuntimeException
-{
-	private $name;
-
-	public function setName($name)
-	{
-		$this->name = $name;
-	}
-}
+class ProxyExistsException extends \RuntimeException {}
