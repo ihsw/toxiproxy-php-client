a8022d7aba2ab40242cce4a656a01122ee1612d2
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-01 20:50:01 -0400

    - bullshit

--- examples/create.php
+++ examples/create.php
@@ -6,4 +6,5 @@ use Ihsw\Toxiproxy\Toxiproxy;
 
 $toxiproxy = new Toxiproxy();
 $proxy = $toxiproxy->create("ihsw_example_redis_master", "localhost:6379", "localhost:43434");
-$proxy->update("jitter", "downstream", ["jitter" => 100]);
+$proxy->update("latency", "upstream", ["latency" => 1000]);
+$proxy->update("latency", "downstream", ["latency" => 1000]);
