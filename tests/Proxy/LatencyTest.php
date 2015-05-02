0271185f7197995e974a0975467ef31f9c6f6130
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-01 20:50:13 -0400

    - migrating latency-related tests to its own file

--- tests/Proxy/LatencyTest.php
+++ tests/Proxy/LatencyTest.php
@@ -4,7 +4,7 @@ use GuzzleHttp\Client as HttpClient;
 use Ihsw\Toxiproxy\Toxiproxy,
     Ihsw\Toxiproxy\Proxy;
 
-class ProxyTest extends \PHPUnit_Framework_TestCase
+class LatencyTest extends \PHPUnit_Framework_TestCase
 {
     const TEST_NAME = "ihsw_test_redis_master";
     const TEST_UPSTREAM = "localhost:6379";
@@ -42,17 +42,41 @@ class ProxyTest extends \PHPUnit_Framework_TestCase
         $callback($proxy);
     }
 
-    /**
-     * @expectedException Ihsw\Toxiproxy\Exception\InvalidToxicException
-     */
-    public function testUpdateInvalidToxic()
+    public function testUpdateLatencyDownstream()
     {
         $this->handleProxy(function(Proxy $proxy){
-            $response = $proxy->update("fdsfgs", "downstream", []);
+            $response = $proxy->update("latency", "downstream", ["latency" => 100]);
             $this->assertEquals(
                 $response->getStatusCode(),
                 Toxiproxy::OK,
-                sprintf("Could not update downstream fdsfgs toxic for proxy '%s'", $proxy["name"])
+                sprintf("Could not update downstream latency toxic for proxy '%s'", $proxy["name"])
+            );
+        });
+    }
+
+    public function testUpdateLatencyUpstream()
+    {
+        $this->handleProxy(function(Proxy $proxy){
+            $response = $proxy->update("latency", "upstream", ["latency" => 100]);
+            $this->assertEquals(
+                $response->getStatusCode(),
+                Toxiproxy::OK,
+                sprintf("Could not update upstream latency toxic for proxy '%s'", $proxy["name"])
+            );
+        });
+    }
+
+    /**
+     * @expectedException Ihsw\Toxiproxy\Exception\NotFoundException
+     */
+    public function testUpdateLatencyInvalidDirection()
+    {
+        $this->handleProxy(function(Proxy $proxy){
+            $response = $proxy->update("latency", "fdsfgs", ["latency" => 100]);
+            $this->assertEquals(
+                $response->getStatusCode(),
+                Toxiproxy::OK,
+                sprintf("Could not update fdsfgs latency toxic for proxy '%s'", $proxy["name"])
             );
         });
     }
