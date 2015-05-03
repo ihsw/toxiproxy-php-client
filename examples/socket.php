1a373de5de535ebc31c7bba3d685428290445541
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-03 13:33:17 -0400

    - helpful comments

--- examples/socket.php
+++ examples/socket.php
@@ -9,11 +9,13 @@ use React\EventLoop\Factory as EventLoopFactory,
     React\Stream\Stream;
 
 $canConnect = function($ip, $port){
+    // misc
     $loop = EventLoopFactory::create();
     $dnsResolverFactory = new DnsResolverFactory();
     $dns = $dnsResolverFactory->createCached("8.8.8.8", $loop); // dunno why dns is required for this shit
-
     $connector = new ClientConnector($loop, $dns);
+
+    // socket loop
     $promise = $connector->create($ip, $port)->then(function (Stream $stream) use ($loop) {
         $stream->close();
         return true;
@@ -21,12 +23,15 @@ $canConnect = function($ip, $port){
         return false;
     });
 
+    // starting it up
     $loop->run();
 
+    // catching the output
     $out = null;
     $promise->done(function($v) use(&$out){
         $out = $v;
     });
+
     return $out;
 };
 
