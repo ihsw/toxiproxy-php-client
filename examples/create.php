00c4ebd37752006b819d34758674717ba78f9cb2
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-03 14:03:19 -0400

    - fixing broken

--- examples/create.php
+++ examples/create.php
@@ -6,4 +6,4 @@ use Ihsw\Toxiproxy\Toxiproxy;
 
 $toxiproxy = new Toxiproxy();
 $proxy = $toxiproxy->create("ihsw_example_redis_master", "localhost:6379", "localhost:43434");
-$proxy->update("timeout", "downstream", ["delay" => 1000, "enabled" => true]);
+$proxy->updateDownstream("timeout", ["delay" => 1000, "enabled" => true]);
