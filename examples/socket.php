95fb7bfaabb2d09117f30f181bfce7b2e83e71aa
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-07 12:57:21 -0400

    - continuing socket experimentation

--- examples/socket.php
+++ examples/socket.php
@@ -4,35 +4,40 @@ require("./vendor/autoload.php");
 
 use React\EventLoop\Factory as EventLoopFactory,
     React\Dns\Resolver\Factory as DnsResolverFactory,
-    React\SocketClient\Connector as ClientConnector,
+    React\Socket\Server,
+    React\SocketClient\Connector,
     React\SocketClient\ConnectionException,
     React\Stream\Stream;
 
-$canConnect = function($ip, $port) {
-    // misc
-    $loop = EventLoopFactory::create();
-    $dnsResolverFactory = new DnsResolverFactory();
-    $dns = $dnsResolverFactory->createCached("8.8.8.8", $loop); // dunno why dns is required for this shit
-    $connector = new ClientConnector($loop, $dns);
+// misc
+$ip = "127.0.0.1";
+$port = 43434;
 
-    // socket loop definition
-    $promise = $connector->create($ip, $port)->then(function (Stream $stream) {
-        $stream->close();
-        return true;
-    }, function(ConnectionException $e) {
-        return false;
-    });
+// server setup
+$serverLoop = EventLoopFactory::create();
+$server = new Server($serverLoop);
+$server->listen($port);
 
-    // starting it up
-    $loop->run();
+// client setup
+$clientLoop = EventLoopFactory::create();
+$dnsResolverFactory = new DnsResolverFactory();
+$dns = $dnsResolverFactory->createCached("8.8.8.8", $clientLoop); // dunno why dns is required for this shit
+$connector = new Connector($clientLoop, $dns);
+$promise = $connector->create($ip, $port)->then(function (Stream $stream) {
+    $stream->close();
+    return true;
+}, function(ConnectionException $e) {
+    return false;
+});
+$clientLoop->run();
 
-    // catching the output
-    $out = null;
-    $promise->done(function($v) use(&$out) {
-        $out = $v;
-    });
+// catching the output
+$out = null;
+$promise->done(function($v) use(&$out) {
+    $out = $v;
+});
 
-    return $out;
-};
+// cleanup
+$server->shutdown();
 
-var_dump($canConnect("127.0.0.1", 43434));
+var_dump($out);
