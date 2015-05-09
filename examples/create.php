22a22378f4c229d711c6477413602ec2ba471396
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-09 17:45:52 -0400

    - making the create example runnable multiple times

--- examples/create.php
+++ examples/create.php
@@ -6,4 +6,8 @@ use GuzzleHttp\Client as HttpClient;
 use Ihsw\Toxiproxy\Toxiproxy;
 
 $toxiproxy = new Toxiproxy(new HttpClient(["base_url" => "http://127.0.0.1:8474"]));
-$proxy = $toxiproxy->create("ihsw_example_redis_master", "127.0.0.1:6379", "127.0.0.1:43434");
+$name = "ihsw_example_redis_master";
+$proxy = $toxiproxy->get($name);
+if (is_null($proxy)) {
+	$proxy = $toxiproxy->create($name, "127.0.0.1:6379", "127.0.0.1:43434");
+}
