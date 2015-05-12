cf9841c6c44a2eae767be1b0f2de2b9044ee8473
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-11 21:27:31 -0400

    - helpful examples

--- examples/create.php
+++ examples/create.php
@@ -6,8 +6,9 @@ use GuzzleHttp\Client as HttpClient;
 use Ihsw\Toxiproxy\Toxiproxy;
 
 $toxiproxy = new Toxiproxy(new HttpClient(["base_url" => "http://127.0.0.1:8474"]));
-$name = "ihsw_example_redis_master";
-$proxy = $toxiproxy->get($name);
-if (is_null($proxy)) {
-	$proxy = $toxiproxy->create($name, "127.0.0.1:6379", "127.0.0.1:43434");
-}
+$proxy = $toxiproxy->create("ihsw_example_redis_master", "127.0.0.1:6379", "127.0.0.1:42424");
+printf(
+	"Listening on IP %s and port %s on behalf of 6379\n",
+	$proxy->getListenIp(),
+	$proxy->getListenPort()
+);
