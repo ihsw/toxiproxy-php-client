553a408fc5d1bdc7113dbd1716935feb04ab22c3
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-05-11 20:56:37 -0400

    - adding helpful

--- src/Proxy.php
+++ src/Proxy.php
@@ -60,6 +60,20 @@ class Proxy implements \ArrayAccess
         return $this;
     }
     public function getListen() { return $this->listen; }
+    public function getListenIp()
+    {
+        $ip = implode(":", explode(":", $this->listen, -1));
+        if (substr($ip, 0, 1) === "[") {
+            $ip = substr($ip, 1, -1);
+        }
+        return $ip;
+    }
+    public function getListenPort()
+    {
+        $ip = $this->getListenIp();
+        $start = substr($this->listen, 0, 1) === "[" ? 3 : 1;
+        return substr($this->listen, $start + strlen($ip));
+    }
 
     public function setUpstreamToxics(array $toxics)
     {
