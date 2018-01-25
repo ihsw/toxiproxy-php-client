e0cae55cae76538aed0e9f44b1d97a29e1a23485
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-01-25 12:17:29 -0500

    Add composer.json file for examples, update bandwidth example with new intended usage.

--- examples/bandwidth.php
+++ examples/bandwidth.php
@@ -2,15 +2,18 @@
 
 require("./vendor/autoload.php");
 
-use GuzzleHttp\Client as HttpClient;
 use Ihsw\Toxiproxy\Toxiproxy;
+use Ihsw\Toxiproxy\ToxicTypes;
 
-// hooking up to toxiproxy and starting a bandwidth proxy
-$toxiproxy = new Toxiproxy(new HttpClient(["base_url" => "http://127.0.0.1:8474"]));
+$toxiproxy = new Toxiproxy("http://toxiproxy:8474");
 $proxy = $toxiproxy->create("ihsw_example_redis_master", "127.0.0.1:6379");
-$proxy->updateDownstream("bandwidth", ["enabled" => true, "rate" => 1000]);
+$toxic = $proxy->create(ToxicTypes::BANDWIDTH, "upstream", 1.0, [
+    "rate" => 1000
+]);
 printf(
     "Listening on IP %s and port %s on behalf of 6379, with a connection that's limited to 1000KB/s\n",
     $proxy->getListenIp(),
     $proxy->getListenPort()
 );
+
+$toxiproxy->delete($proxy);
